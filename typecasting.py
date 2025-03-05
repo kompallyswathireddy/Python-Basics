3966c8d9195eae20428044261fbb6af6e4004a71
kompallyswathireddy <swathireddyb1996@gmail.com>
2025-03-05 23:09:48 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,22 @@
+#Assigning Diffrent variables
+name='swathi'
+age=28
+is_student=True
+weight=31.9
+#printing diffrent variables and their data type
+print('name :',name) 
+print('data type of name is :',type(name))
+print('age :',age) 
+print('data type of age is :',type(age))
+print('is_student :',is_student) 
+print('data type of is_student is :',type(is_student))
+print('weight :',weight) 
+print('data type of weight is :',type(weight))
+#Type casting to convert the datatype of the variables
+print('\n after type casting')
+age=str(age)
+print(age)
+print('data type of age is',type(age) )
+weight=int(weight)
+print(weight)
+print('data type of weight is',type(weight) )
