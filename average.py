6ef33212d83a489cd25f5128fe973d0d7c07f239
kompallyswathireddy <swathireddyb1996@gmail.com>
2025-03-05 23:19:56 +0530

    average

---
+++ average.py
@@ -0,0 +1,12 @@
+#storing values
+tree1=38
+tree2=57
+tree3=45
+tree4=31
+tree5=65
+#finding the tottal of trees
+sum=tree1+tree2+tree3+tree4+tree5
+print('the sum of all the 5 trees is:',sum)
+#finding the average of trees
+average=sum/5
+print('the average of the trees is:',average)
