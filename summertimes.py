7e495b56202e3fd74623048b361909b24eb0d2b5
kompallyswathireddy <swathireddyb1996@gmail.com>
2025-03-06 00:22:56 +0530

    summer times

---
+++ summertimes.py
@@ -0,0 +1,10 @@
+temperature_celsius = 30  # Example temperature in Celsius
+
+if temperature_celsius > 30:
+    print("It's very hot! Wear shorts and a t-shirt.")
+elif 20 < temperature_celsius <= 30:
+    print("It's warm! A t-shirt and jeans would be perfect.")
+elif 15 < temperature_celsius <= 20:
+    print("It's mild! A light sweater and pants would work well.")
+else:
+    print("It's a bit cool for summer. Wear a jacket.")
