50e3d88cb01103eab8354075ba12bbdafcebcbe4
kompallyswathireddy <swathireddyb1996@gmail.com>
2025-03-05 22:51:17 +0530

    datatypes

---
+++ Datatypes.py
@@ -0,0 +1,9 @@
+# data types of diffrent values
+a=3
+print('type of a :',type(a))
+b=1.55
+print('type of b :',type(b))
+c='swathi'
+print('type of c:',type(c))
+d=True
+print('type of d:',type(d))
